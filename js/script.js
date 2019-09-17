7b400a91d2a007ba659fcc7d277b33309ea6fc13
Manny <manny-apsel@hotmail.com>
2019-09-17 17:01:14 +0200

    managed to get translated text

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 document.getElementById("idForm").addEventListener("submit", function () {
     console.log(document.getElementById("idSearchBar").value);
 });
-console.log("aaa");
+
+
+
+fetch("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190917T143143Z.c1229abdc907b884.5520b5430c18e7eb1926dd0166c1bd73473c23f0&text=Show syntactic highlighting&lang=nl")
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+});
