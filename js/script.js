ab6def51645e2ce53ad74a93bdaac6f3c7274a8b
bermarte <bermarte@hotmail.com>
2019-09-18 11:38:07 +0200

    fixing CORS issue

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-var song ="";
-var artist="";
+document.getElementById("idForm").addEventListener("submit", function () {
+    console.log(document.getElementById("idSearchBar").value);
+});
+
+
 document.getElementById("idForm").addEventListener("submit", function () {
     song = document.getElementById("idSearchBar").value;
     artist = document.getElementById("artist").value;
     getData(artist, song);
 });
 
+
 //fetch search song
 function getData(artist, song) {
 
-    console.log("https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist="+artist+"&song="+song+"");
-    fetch("https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad", {
+    artist = artist.replace(/\s/g, "%2520");
+    song = song.replace(/\s/g, "%2520");
+
+    //michael%2520jackson
+    url = "https://api-gateway-becode.herokuapp.com/?goto=http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect%3Fartist%3D"+artist+"%26song%3D"+song;
+    //right one: https://api-gateway-becode.herokuapp.com/?goto=http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect%3Fartist%3Dmichael%2520jackson%26song%3Dthriller
+    console.log(url);
+    fetch(url, {
         "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "sridurgayadav-chart-lyrics-v1.p.rapidapi.com",
-            "x-rapidapi-key": "ee05e32eabmshb0e8bb69aff436ap10a280jsnd9466639d2a3"
-        }
     })
         .then(response => {
-            //console.log(response);
+            console.log(url)
             return response.text();
         })
+
         .then(function (data) {
+
             let parser = new DOMParser(),
                 xmlDoc = parser.parseFromString(data, 'text/xml');
 
             text = xmlDoc.getElementsByTagName('Lyric')[0].innerHTML;
+            //text = xmlDoc.getElementsByTagName('Lyric')[0].childNodes[0];
+
             document.getElementById("results").innerHTML = "<pre>"+text+"</pre>";
 
 
